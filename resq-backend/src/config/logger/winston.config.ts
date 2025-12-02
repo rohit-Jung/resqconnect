84692e59cdbed5e88e351e5fe465c00ef5ec0e36
rohitJung <rokshh28@gmail.com>
2025-12-02 11:02:36 +0545

    feat(backend:config): morgan and winston

---
+++ resq-backend/src/config/logger/winston.config.ts
@@ -0,0 +1,53 @@
+import winston from "winston";
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+  verbose: 5,
+};
+
+const level = () => {
+  const env = process.env.NODE_ENV || "development";
+  const isDevelopment = env === "development";
+  return isDevelopment ? "debug" : "warn";
+};
+
+const colors = {
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  debug: "white",
+  verbose: "cyan",
+};
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+  new winston.transports.File({ filename: "logs/combined.log" }),
+  new winston.transports.File({ filename: "logs/info.log", level: "info" }),
+  new winston.transports.File({ filename: "logs/debug.log", level: "debug" }),
+];
+
+const format = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    ({ timestamp, level, message }) =>
+      `${timestamp} [${level.toUpperCase()}]: ${message}`,
+  ),
+);
+
+winston.addColors(colors);
+
+const logger = winston.createLogger({
+  level: level(),
+  levels,
+  format,
+  transports,
+});
+
+export { logger };
