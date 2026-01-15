82a26df04b15294e62cf1ea7ee7f1368c83b44a6
rohitJung <rokshh28@gmail.com>
2026-01-15 11:02:57 +0545

    chore: add middlewares

--- resq-backend/src/middlewares/auth.middleware.ts
+++ resq-backend/src/middlewares/auth.middleware.ts
@@ -1,23 +1,9 @@
-import type { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from 'express';
 
-import type { TUser, TUserRole } from "@/models";
-import ApiError from "@/utils/api/ApiError";
-import { asyncHandler } from "@/utils/api/asyncHandler";
-import { verifyJWT } from "@/utils/tokens/jwtTokens";
-
-const verifyAndDecodeToken = (token: string) => {
-  if (!token) {
-    throw new ApiError(401, "Unauthorized");
-  }
-
-  const decoded = verifyJWT(token) as Partial<TUser>;
-
-  if (!decoded.id) {
-    throw new ApiError(401, "Invalid token");
-  }
-
-  return decoded;
-};
+import type { TUserRole } from '@/models';
+import ApiError from '@/utils/api/ApiError';
+import { asyncHandler } from '@/utils/api/asyncHandler';
+import { verifyAndDecodeToken } from '@/utils/tokens/jwtTokens';
 
 const validateServiceProvider = asyncHandler(
   async function validateServiceProvider(
@@ -26,30 +12,51 @@ const validateServiceProvider = asyncHandler(
     next: NextFunction,
   ) {
     const token =
-      req.cookies.token || req.headers.authorization?.replace("Bearer ", "");
+      req.cookies.token || req.headers.authorization?.replace('Bearer ', '');
 
-    const decoded = verifyAndDecodeToken(token);
-    req.user = decoded;
+    const decoded = await verifyAndDecodeToken(token);
+    if (decoded && decoded != null) {
+      req.user = decoded;
+    }
+
     next();
   },
 );
 
 const validateRoleAuth = (allowedRoles: TUserRole[]) => {
-  return asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const token =
-        req.cookies.token || req.headers.authorization?.replace("Bearer ", "");
+  return asyncHandler(async (req: Request, _, next: NextFunction) => {
+    const token =
+      req.cookies.token || req.headers.authorization?.replace('Bearer ', '');
 
-      const decoded = verifyAndDecodeToken(token);
-
-      if (!allowedRoles.includes(decoded.role as TUserRole)) {
-        throw new ApiError(401, "Not authorized to perform this action");
-      }
-
-      req.user = decoded;
+    const decoded = await verifyAndDecodeToken(token);
+    if (!decoded || decoded == null) {
       next();
-    },
-  );
+      return;
+    }
+
+    if (!allowedRoles.includes(decoded.role as TUserRole)) {
+      throw new ApiError(401, 'Not authorized to perform this action');
+    }
+
+    req.user = decoded;
+    next();
+  });
 };
 
-export { validateServiceProvider, validateRoleAuth };
+const validateOrg = asyncHandler(
+  async (req: Request, _, next: NextFunction) => {
+    const token =
+      req.cookies.token || req.headers.authorization?.replace('Bearer ', '');
+
+    const decoded = await verifyAndDecodeToken(token);
+    if (!decoded || decoded == null) {
+      next();
+      return;
+    }
+
+    req.user = decoded;
+    next();
+  },
+);
+
+export { validateServiceProvider, validateRoleAuth, validateOrg };
