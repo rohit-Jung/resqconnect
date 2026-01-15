012d40b5ae7095ad2408c1fa45b63a3aebc9cf1f
rohitJung <rokshh28@gmail.com>
2026-01-15 11:00:56 +0545

    feat(config): add logger and other

--- resq-backend/src/config/logger/morgan.config.ts
+++ resq-backend/src/config/logger/morgan.config.ts
@@ -1,14 +1,14 @@
-import morgan from "morgan";
+import morgan from 'morgan';
 
-import { logger } from "./winston.config.js";
+import { logger } from './winston.config.js';
 
 const stream = {
   write: (message: string) => logger.http(message.trim()),
 };
 
 const morganConfig = morgan(
-  ":method :url :status :res[content-length] - :response-time ms",
-  { stream },
+  ':method :url :status :res[content-length] - :response-time ms',
+  { stream }
 );
 
 export { morganConfig };
