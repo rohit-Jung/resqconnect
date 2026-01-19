dbede824bc31cadf1f7d959918fd5c9ef6c12a5d
rohitJung <rokshh28@gmail.com>
2026-01-19 14:44:55 +0545

    refactor: updated code

--- resq-backend/type.d.ts
+++ resq-backend/type.d.ts
@@ -1,9 +1,16 @@
-import type { TUser } from "@/models";
+import type { TUser } from '@/models';
+import type { IJWTToken } from '@/utils/tokens/jwtTokens';
 
 declare global {
   namespace Express {
     interface Request {
-      user: Partial<TUser>;
+      user: IJWTToken;
     }
+  }
+}
+
+declare module 'socket.io' {
+  interface Socket {
+    user: IJWTToken;
   }
 }
