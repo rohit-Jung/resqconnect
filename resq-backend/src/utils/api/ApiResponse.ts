db1b35844c437fa775e95386a1baab47ae00e816
rohitJung <rokshh28@gmail.com>
2026-01-31 16:09:31 +0545

    fix: handle big int

--- resq-backend/src/utils/api/ApiResponse.ts
+++ resq-backend/src/utils/api/ApiResponse.ts
@@ -7,33 +7,33 @@ class ApiResponse {
   constructor(statusCode: number, message: string = 'success', data: any) {
     this.statusCode = statusCode;
     this.message = message;
-    this.data = data;
+    this.data = this.handleBigInt(data);
     this.success = statusCode < 400;
   }
 
-  // private handleBigInt(data: any): any {
-  //   if (data === null || data === undefined) return data;
+  private handleBigInt(data: any): any {
+    if (data === null || data === undefined) return data;
 
-  //   if (typeof data === "bigint") {
-  //     return data.toString();
-  //   }
+    if (typeof data === 'bigint') {
+      return data.toString();
+    }
 
-  //   if (Array.isArray(data)) {
-  //     return data.map(this.handleBigInt);
-  //   }
+    if (Array.isArray(data)) {
+      return data.map(this.handleBigInt.bind(this));
+    }
 
-  //   if (typeof data === "object") {
-  //     const result: any = {};
-  //     for (const key in data) {
-  //       if (data.hasOwnProperty(key)) {
-  //         result[key] = this.handleBigInt(data[key]);
-  //       }
-  //     }
-  //     return result;
-  //   }
+    if (typeof data === 'object') {
+      const result: any = {};
+      for (const key in data) {
+        if (Object.prototype.hasOwnProperty.call(data, key)) {
+          result[key] = this.handleBigInt(data[key]);
+        }
+      }
+      return result;
+    }
 
-  //   return data;
-  // }
+    return data;
+  }
 }
 
 export default ApiResponse;
