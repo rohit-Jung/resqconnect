3e3b5149b6d60495f0d8fc01ab4c97b8e3efb90c
rohitJung <rokshh28@gmail.com>
2026-01-31 16:10:59 +0545

    refactor: use fonts and check auth

--- resq-mobile/app/_layout.tsx
+++ resq-mobile/app/_layout.tsx
@@ -1,49 +1,79 @@
 import '../global.css';
 
-import {
-  ChauPhilomeneOne_400Regular,
-  ChauPhilomeneOne_400Regular_Italic,
-} from '@expo-google-fonts/chau-philomene-one';
-import {
-  Inter_100Thin,
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold,
-  Inter_900Black,
-} from '@expo-google-fonts/inter';
 import TanstackQueryClientProvider from '@/providers/react-query.provider';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import { useEffect, useState, useCallback } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { useLoadFonts } from '@/hooks/useLoadFonts';
+import { connectToSocketServer } from '@/socket';
+import { useAuthStore } from '@/store/authStore';
+import { TOKEN_KEY } from '@/constants';
+import api from '@/services/axiosInstance';
+import { userEndpoints } from '@/services/endPoints';
 
 export { ErrorBoundary } from 'expo-router';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    ChauPhilomeneOne_400Regular,
-    ChauPhilomeneOne_400Regular_Italic,
-    Inter_100Thin,
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_800ExtraBold,
-    Inter_900Black,
-  });
+  const [loaded, error] = useLoadFonts();
+  const { user, setUser, setLoading, isLoading } = useAuthStore();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+  const deleteUserInfo = async () => {
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+    setUser(null);
+  }
 
   useEffect(() => {
-    if (loaded || error) {
+    const checkAuthAndFetchProfile = async () => {
+      try {
+        const token = await SecureStore.getItemAsync(TOKEN_KEY);
+        console.log(":Profile Response", token, user);
+        if (token && !user) {
+          const response = await api.get(userEndpoints.profile);
+          console.log(":Profile Response", response);
+          if (!response.data?.data?.user) await deleteUserInfo();
+          setUser(response.data.data.user);
+        }
+      } catch (err) {
+        await deleteUserInfo();
+      } finally {
+        setIsCheckingAuth(false);
+        setLoading(false);
+      }
+    };
+
+    checkAuthAndFetchProfile();
+  }, []);
+
+  useEffect(() => {
+    if ((loaded || error) && !isCheckingAuth) {
       SplashScreen.hideAsync();
     }
-  }, [loaded, error]);
+  }, [loaded, error, isCheckingAuth]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const initSocket = async () => {
+      const token = await SecureStore.getItemAsync(TOKEN_KEY);
+      if (token) {
+        connectToSocketServer({ token });
+      }
+    };
+
+    initSocket();
+  }, [user]);
 
   if (!loaded && !error) {
+    return null;
+  }
+
+  if (isCheckingAuth || isLoading) {
     return null;
   }
 
