f80b9fbe2cb51cd708f28d60b34063634fab6526
rohitJung <rokshh28@gmail.com>
2026-04-09 16:24:12 +0545

    fix: drizzle migrations messed up

--- apps/backend/drizzle/migrations/0006_flawless_maelstrom.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "request_events" DROP CONSTRAINT "request_events_request_id_emergency_request_id_fk";
---> statement-breakpoint
-ALTER TABLE "request_events" ADD CONSTRAINT "request_events_request_id_emergency_request_id_fk" FOREIGN KEY ("request_id") REFERENCES "public"."emergency_request"("id") ON DELETE cascade ON UPDATE no action;
