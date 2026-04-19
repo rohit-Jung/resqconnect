4045be8ec215a0801c79d4168eba60c8e26672ce
rohitJung <rokshh28@gmail.com>
2026-04-19 11:13:50 +0545

    feat: add sms function and webhooks

--- apps/backend/src/routes/v1/healthcheck.routes.ts
+++ apps/backend/src/routes/v1/healthcheck.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
-import { healthCheck } from '@/controllers/healthcheck.controller';
+import {
+  healthCheck,
+  workerHealthCheck,
+} from '@/controllers/healthcheck.controller';
 
 const healthCheckRouter = Router();
 
 healthCheckRouter.route('/').get(healthCheck);
+healthCheckRouter.route('/workers').get(workerHealthCheck);
+healthCheckRouter.route('/workers/sms').get(workerHealthCheck);
 
 export default healthCheckRouter;
