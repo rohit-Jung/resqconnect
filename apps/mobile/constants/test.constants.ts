7f14c32fbba19f5efa08061da1371ae6d41278ba
rohitJung <rokshh28@gmail.com>
2026-05-01 10:48:37 +0545

    remove: mobile app (separated)

--- apps/mobile/constants/test.constants.ts
+++
@@ -1,5 +0,0 @@
-// test Coordinates
-export const TEST_CORDS = {
-  latitude: 27.7360111836845,
-  longitude: 85.37725755926047,
-};
--- apps/mobile/socket/socket-connection.ts
+++
@@ -1,9 +0,0 @@
-import { socketManager } from './socket-manager';
-
-export function connectToSocketServer({ token }: { token: string | null }) {
-  socketManager.connect(token);
-}
-
-export function disconnectSocket() {
-  socketManager.disconnect();
-}
